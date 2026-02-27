73a1b1426b218b395fd22ff3dc84ddef26076f90
Arina <arinasmakh@gmail.com>
2026-02-27 16:44:01 +0300

    Updating main.py

--- app/main.py
+++ app/main.py
@@ -1,1 +1,14 @@
-print('Hello, world')
+from fastapi import FastAPI
+from app.routers import categories
+
+app = FastAPI(
+    title='Интернет-магазин на FastAPI',
+    version='0.1.0'
+)
+
+app.include_router(categories.router)
+
+
+@app.get('/')
+async def root():
+    return {'message': 'Добро пожаловать в API интернет-магазина'}
