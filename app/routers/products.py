121500a69e1dc6a1d23132f1f04e3b2af0afbc7c
Arina <arinasmakh@gmail.com>
2026-03-05 17:19:51 +0300

    HTTP endpoint POST in products

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, HTTPException
+from sqlalchemy import select, update
+from sqlalchemy.orm import Session
+
+from app.db_depends import get_db
+from app.schema import Product as ProductSchema, ProductCreate
+from app.models.products import Product as ProductModel
+from app.models.categories import Category as CategoryModel
 
 router = APIRouter(
     prefix='/products',
@@ -14,11 +21,22 @@ async def get_all_products():
     return {'message': 'Список всех продуктов.'}
 
 
-@router.post('/')
-async def create_product():
+@router.post('/', status_code=status.HTTP_201_CREATED, response_model=ProductSchema)  # определяем код ответа и модель ответа
+async def create_product(product: ProductCreate, db: Session = Depends(get_db)):  # модель для создания и БД
     '''
     To create a new product
     '''
+    # проверяем, что такая категория существует
+    stmt_category_id = select(CategoryModel.id).where(CategoryModel.is_active==True, CategoryModel.id == product.category_id)
+    res_category_id = db.scalars(stmt_category_id).all()
+    if res_category_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Category not found or inactive')
+    new_product = ProductModel(**product.model_dump())
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+    return new_product
+
     return {'message': 'Товар создан.'}
 
 
