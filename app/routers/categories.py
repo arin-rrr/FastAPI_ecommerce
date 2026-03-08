be522548d9de418b78e738500303c710edbf9b1c
Arina <arinasmakh@gmail.com>
2026-03-08 22:46:49 +0300

    Final changes in categories async endpoints

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from app.db_depends import get_async_db
 from app.models.categories import Category as CategoryModel
 from app.schema import Category as CategorySchema, CategoryCreate
-from app.db_depends import get_db
 
 router = APIRouter(
     prefix='/categories',
