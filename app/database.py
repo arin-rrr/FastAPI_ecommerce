3350b14240c4e2230f372576a0a055792d5d25f8
Arina <arinasmakh@gmail.com>
2026-03-08 20:26:01 +0300

    Connecting PostgreSQL

--- app/database.py
+++ app/database.py
@@ -1,11 +1,31 @@
-from sqlalchemy import create_engine, Column, Integer, String, Float, Boolean, DateTime
-from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, DateTime
 from datetime import datetime
+from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'sqlite:///ecommerce.db'
+
+# Строка подключения для SQLite
+DATABASE_URL = "sqlite:///ecommerce.db"
+
+# Создаём Engine
 engine = create_engine(DATABASE_URL, echo=True)
 
+# Настраиваем фабрику сеансов
 SessionLocal = sessionmaker(bind=engine)
+
+
+# --------------- Асинхронное подключение к PostgreSQL -------------------------
+
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase
+
+# Строка подключения для PostgreSQl
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:xxxxxxxx@localhost:5432/ecommerce_db"
+
+# Создаём Engine
+async_engine = create_async_engine(DATABASE_URL, echo=True)
+
+# Настраиваем фабрику сеансов
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False, class_=AsyncSession)
 
 
 class Base(DeclarativeBase):
