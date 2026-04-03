ef72e98052ae93ef48c6b3c3be9666c2dd753d91
Arina <arinasmakh@gmail.com>
2026-04-03 17:15:24 +0300

    Added Cart

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from app.models.categories import Category
 from app.models.products import Product
 from app.models.users import User
+from app.models.cart_item import CartItem
 
-__all__ = ['Category', 'Product', 'User']
+__all__ = ['Category', 'Product', 'User', 'CartItem']
