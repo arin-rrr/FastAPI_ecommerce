7377724dc58345710fb95ec3f6ccc2631f701de9
Arina <arinasmakh@gmail.com>
2026-04-04 18:06:23 +0300

    Added Orders and OrderItems

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from app.models.categories import Category
 from app.models.products import Product
 from app.models.users import User
 from app.models.cart_item import CartItem
+from app.models.orders import Order, OrderItem
 
-__all__ = ['Category', 'Product', 'User', 'CartItem']
+__all__ = ['Category', 'Product', 'User', 'CartItem', 'Order', 'OrderItem']
--- app/models/products.py
+++ app/models/products.py
@@ -39,6 +39,7 @@ class Product(Base):
     category: Mapped["Category"] = relationship("app.models.categories.Category", back_populates='products')
     seller: Mapped["User"] = relationship("User", back_populates="products")
     cart_items: Mapped[list['CartItem']] = relationship('CartItem', back_populates='product', cascade='all, delete-orphan')
+    order_items: Mapped[list['OrderItem']] = relationship('OrderItem', back_populates='product')
     __table_args__ = (
         Index('ix_products_tsv_gin', 'tsv', postgresql_using="gin"),
     )
