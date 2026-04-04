3181371a67df40dcbc3fe6a8229648bbf12ec7ad
Arina <arinasmakh@gmail.com>
2026-04-04 18:55:51 +0300

    Added info by order_id endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import categories, products, users, reviews, cart
+from app.routers import categories, products, users, reviews, cart, orders
 
 app = FastAPI(
     title='Интернет-магазин на FastAPI',
@@ -11,6 +11,7 @@ app.include_router(products.router)
 app.include_router(users.router)
 app.include_router(reviews.router)
 app.include_router(cart.router)
+app.include_router(orders.router)
 
 
 @app.get('/')
